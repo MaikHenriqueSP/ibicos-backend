5fb5242b1e43da1a31e3c096bc2d95e8e877c888
Maik Henrique <maik.hsp.contato@gmail.com>
2020-10-31 10:06:46 -0300

    feat(base project): first commit
    - set of the needed application dependencies.

---
+++ src/main/java/br/com/ibicos/ibicos/IbicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibicos.ibicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ibicos/ibicos/IbicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ibicos.ibicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
