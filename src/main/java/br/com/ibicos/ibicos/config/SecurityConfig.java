6c08f877eadebf4539131b9117c128c5bcea74c0
Maik Henrique <maik.hsp.contato@gmail.com>
2020-11-15 21:48:26 -0300

    feat(documentation): implementation of the API documentation
    - implementation of the API documentation using spring swagger along with basic configuration

--- src/main/java/br/com/ibicos/ibicos/config/SecurityConfig.java
+++ src/main/java/br/com/ibicos/ibicos/config/SecurityConfig.java
@@ -29,6 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	private static final String[] AUTH_WHITELIST = { "/login", "/signUp", "/recover", "/verify", "/resetPassword/**",
+			"/v2/api-docs", "/swagger-resources", "/swagger-resources/**",
+
+			"/configuration/ui", "/configuration/security", "/swagger-ui.html", "/webjars/**" };
+
 	public OncePerRequestFilter jwtFilter() {
 		return new JwtAuthFilter(jwtService, userDetailsServiceImpl);
 	}
@@ -39,9 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.csrf().disable().authorizeRequests()
-				.antMatchers("/login", "/signUp", "/recover", "/verify", "/resetPassword/**").permitAll().anyRequest()
-				.authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		http.csrf().disable().authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll().anyRequest().authenticated()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
 	}
