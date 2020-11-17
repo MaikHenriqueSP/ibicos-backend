f45678ded89aad6b0aced30e4ccf194bc630b8e5
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2020-11-17 09:58:11 -0300

    feat(repos): add provider statistics and statistics repo

--- src/main/java/br/com/ibicos/ibicos/repository/StatisticsRepository.java
+++ src/main/java/br/com/ibicos/ibicos/repository/StatisticsRepository.java
@@ -1,5 +1,9 @@
 package br.com.ibicos.ibicos.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,6 @@ import br.com.ibicos.ibicos.entity.Statistics;
 
 @Repository
 public interface StatisticsRepository extends JpaRepository<Statistics, Integer> {
-
+	Page<Statistics> findByUserId(Integer userId, Pageable pageable);
+	Optional<Statistics> findByIdAndUserId(Integer statisticsId, Integer userId);
 }
