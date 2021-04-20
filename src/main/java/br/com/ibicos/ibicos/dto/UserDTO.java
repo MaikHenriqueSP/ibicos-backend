859344ed6ba6af436c4f39c5e6e762a14d5f88c0
Maik Henrique <maik.hsp.contato@gmail.com>
2021-04-20 17:07:36 -0300

    feat(user): creation of user dto holder

--- src/main/java/br/com/ibicos/ibicos/dto/UserDTO.java
+++ src/main/java/br/com/ibicos/ibicos/dto/UserDTO.java
@@ -1,8 +1,5 @@
 package br.com.ibicos.ibicos.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -10,20 +7,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonRootName(value = "ad")
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class AdDTO {
-
+public class UserDTO {
     private Integer id;
-    private String adDescription;
-    private ServiceCategoryDTO serviceCategory;
-    private List<AdCityDTO> cities;
-    private UserDTO user;
-
 }
