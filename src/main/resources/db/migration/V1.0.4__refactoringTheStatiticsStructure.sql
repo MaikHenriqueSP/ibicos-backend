8770335aee8df3478943aa257dd682fbacadf40f
Maik Henrique <maik.hsp.contato@gmail.com>
2021-05-16 14:27:55 -0300

    feat(db): creation of the customer_statistics schema
    - creation as a sub-type of statitics in order to differentiate provider to customer statistics

--- src/main/resources/db/migration/V1.0.4__refactoringTheStatiticsStructure.sql
+++ src/main/resources/db/migration/V1.0.4__refactoringTheStatiticsStructure.sql
@@ -1,2 +1,8 @@
 ALTER TABLE statistics
     DROP COLUMN views_counter;
+
+CREATE TABLE IF NOT EXISTS customer_statistics (
+    id_customer INT PRIMARY KEY AUTO_INCREMENT,
+    fk_id_statistics INT,
+    FOREIGN KEY (fk_id_statistics) REFERENCES statistics(id_statistics)
+);
