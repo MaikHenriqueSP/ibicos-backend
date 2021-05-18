243d29178d4dc08620495252345a38510d750678
Maik Henrique <maik.hsp.contato@gmail.com>
2021-05-18 20:05:41 -0300

    fix(dto): representation of inheritance relation
    - addition of inheritance relation between ProviderStatisticsDTO and StatisticsDTO
    - refactor of @Builder to @SuperBuilder annotation to fix lombok issues

--- src/main/java/br/com/ibicos/ibicos/dto/ProviderStatisticsDTO.java
+++ src/main/java/br/com/ibicos/ibicos/dto/ProviderStatisticsDTO.java
@@ -1,26 +1,18 @@
 package br.com.ibicos.ibicos.dto;
 
-import com.fasterxml.jackson.annotation.JsonRootName;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
+@EqualsAndHashCode(callSuper = true)
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class ProviderStatisticsDTO {
-    private StatisticsDTO statistics;
+@SuperBuilder(toBuilder = true)
+public class ProviderStatisticsDTO extends StatisticsDTO{
     private Integer id;
     private Integer visualizations;
     private ServiceCategoryDTO category;
-
-
 }
