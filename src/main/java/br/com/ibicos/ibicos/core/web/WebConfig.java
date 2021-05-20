710e18e9de8288437dd182cba07aa80cb0a06b21
Maik Henrique <maik.hsp.contato@gmail.com>
2021-05-20 20:01:39 -0300

    refactor(web): move of web configuration into core/web

--- src/main/java/br/com/ibicos/ibicos/core/web/WebConfig.java
+++ src/main/java/br/com/ibicos/ibicos/core/web/WebConfig.java
@@ -1,4 +1,4 @@
-package br.com.ibicos.ibicos.config;
+package br.com.ibicos.ibicos.core.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
